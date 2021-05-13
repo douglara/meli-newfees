62b0df6ad4ab6f0c186ae1f9b3f8f6aa9cb03e49
Douglas Lara <douglasdelaras@gmail.com>
2021-05-13 15:51:22 -0300

    Increment field 'old_fees'

--- pages/api/v1/listing_prices/[id].js
+++ pages/api/v1/listing_prices/[id].js
@@ -19,8 +19,31 @@ async function listing_prices(request,response) {
 
   const category_on_meli = await category_on_meli_request.json()
 
+  response.json({
+    id: category_id,
+    current_fees: category_on_meli,
+    new_fees: get_new_fees(category_id_only_numbers),
+    old_fees: get_old_fees(category_id)
+  });
+}
+
+function get_old_fees(category_id) {
+  const old_fees_json = require('../../../../files/old_fees.json');
+  const old_fees = old_fees_json[`${category_id}`]
+  if (old_fees === undefined){
+    return null
+  }
+  return old_fees
+}
+
+
+function get_new_fees(category_id_only_numbers) {
   const new_fees_json = require('../../../../files/new_fees.json');
   const search = new_fees_json.ML.categoriesFromFile.filter(it => it.L7 === `${category_id_only_numbers}`)[0];
+  if (search === undefined) {
+    return null
+  }
+
   var new_fees_filter = category_on_meli.filter(field => (field.listing_type_id === 'gold_pro' || field.listing_type_id === 'gold_special'));
   var new_fees = new_fees_filter.map(hash => {
 
@@ -32,13 +55,9 @@ async function listing_prices(request,response) {
       new_hash.sale_fee_amount = +search.Clasica
     }
     return new_hash
-    });
-
-  response.json({
-    id: category_id,
-    current_fees: category_on_meli,
-    new_fees: new_fees
   });
+  return new_fees
 }
+
 
 export default listing_prices;
